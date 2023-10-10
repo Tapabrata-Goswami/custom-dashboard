e5545ec74c015706218cfcf48b8df2150bcdfee3
Tapabrata Goswami <tapabratagoswami21@gmail.com>
2023-10-11 04:27:27 +0530

    initial

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+// DATABASE CREDENTIALS
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','');
+
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+
+// SLience is golden.
